236e15e297318781017822df8f52b9e615c8ef02
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-09-18 21:24:22 +0300

    start changing app on hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,67 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Feedback from '../Feedback/Feedback';
 import s from './App.module.css';
 
-class App extends Component {
-  static defaultProps = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: this.props.good,
-    neutral: this.props.neutral,
-    bad: this.props.bad,
-  };
-
-  handleChangeFeedbackCount = evt => {
+  const handleChangeFeedbackCount = evt => {
     if (evt.target.tagName !== 'BUTTON') {
       return;
     }
 
-    this.setState(prevState => ({
-      [evt.target.id]: prevState[evt.target.id] + 1,
-    }));
+    // this.setState(prevState => ({
+    //   [evt.target.id]: prevState[evt.target.id] + 1,
+    // }));
   };
 
-  render() {
-    return (
-      <div className={s.app}>
-        <Feedback
-          feedback={this.state}
-          changeCoutFeedback={this.handleChangeFeedbackCount}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={s.app}>
+      <Feedback
+        feedback={[good, neutral, bad]}
+        changeCoutFeedback={handleChangeFeedbackCount}
+      />
+    </div>
+  );
 }
 
 export default App;
+
+// class App extends Component {
+//   static defaultProps = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   state = {
+//     good: this.props.good,
+//     neutral: this.props.neutral,
+//     bad: this.props.bad,
+//   };
+
+//   handleChangeFeedbackCount = evt => {
+//     if (evt.target.tagName !== 'BUTTON') {
+//       return;
+//     }
+
+//     this.setState(prevState => ({
+//       [evt.target.id]: prevState[evt.target.id] + 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div className={s.app}>
+//         <Feedback
+//           feedback={this.state}
+//           changeCoutFeedback={this.handleChangeFeedbackCount}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;

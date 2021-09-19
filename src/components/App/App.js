37e66707cdff8ec065686e4af80c338a1e58b4e7
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-09-19 07:32:09 +0300

    just commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className={s.app}>
       <Feedback
-        feedback={[good, neutral, bad]}
+        feedback={['good', 'neutral', 'bad']}
         changeCoutFeedback={handleChangeFeedbackCount}
       />
     </div>
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
 export function FeedbackOptions({ feedback, changeCoutFeedback }) {
-  const buttons = Object.keys(feedback);
+  // const buttons = Object.keys(feedback);
 
   return (
     <form className={s.form} onClick={changeCoutFeedback}>
-      {buttons.map(button => {
+      {feedback.map(button => {
         return (
           <button key={button} id={button} type="button" className={s.button}>
             {button}
